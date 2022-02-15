9f24afe3f83d2c2e79f98fe0ba7b127783f05718
unknown <natanlucas771@gmail.com>
2022-02-15 14:27:04 -0300

    adição-de-operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,41 @@
+//Exercicio de interpretação
+
+// 1:
+// A false
+// B false 
+// C true
+// D boolean
+
+// 2:
+//  Será impresso so os numero digitados sem ser feito o calculo.
+// pois estava em string 
+
+// 3:
+//  O erro é que faltou o comando "Number" antes do codigo pra
+// ele ficar como numero.
+// __________________________________________________
+ 
+// exercicio 1 
+
+// let anoNasc = Number(prompt ("Digite sua idade"))
+// let anoAmig = Number(prompt ("Digite a idade do seu melhor amigo"))
+// console.log ("Sua idade é maior do que a do seu melhor amigo?", anoNasc >= anoAmig)
+// console.log ("Diferença de Idade", anoNasc - anoAmig)
+
+// // exercicio 2
+// let numeropar = Number(prompt ("Digite um numero par"))
+// console.log (numeropar % 2)
+// /*Numeros pares sempre dividem e tem que dar 0*/
+// /*se colocar numero impar sempre vai sobrar numeros, e nao vai dar 0 */
+
+// let idadeDeAno = Number(prompt ("Digite sua idade"))
+// console.log ("Anos de vida:", idadeDeAno * 12)
+// console.log ("Dias de vida:", idadeDeAno * 365)
+// console.log ("Horas de vida:", idadeDeAno * 8760)
+
+// let primeiroNumero = Number(prompt ("Digite um numero"))
+// let segundoNumero = Number(prompt ("Digite outro numero"))
+// console.log ("O primeiro numero é maior que segundo?", primeiroNumero >= segundoNumero )
+// console.log ("O primeiro numero é igual ao segundo?", primeiroNumero === segundoNumero )
+// console.log ("O primeiro numero é divisível pelo segundo?", (primeiroNumero / segundoNumero) ===  0 )
+// console.log ("O segundo numero é divisível pelo primeiro?", (segundoNumero / primeiroNumero) ===  0 )
