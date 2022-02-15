937c0c984d696335450273d378ffe75fac9fa7db
Natã Lucas Silva <99276733+NatanBreaks@users.noreply.github.com>
2022-02-15 14:52:53 -0300

    Update index.js

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -37,5 +37,5 @@
 // let segundoNumero = Number(prompt ("Digite outro numero"))
 // console.log ("O primeiro numero é maior que segundo?", primeiroNumero >= segundoNumero )
 // console.log ("O primeiro numero é igual ao segundo?", primeiroNumero === segundoNumero )
-// console.log ("O primeiro numero é divisível pelo segundo?", (primeiroNumero / segundoNumero) ===  0 )
-// console.log ("O segundo numero é divisível pelo primeiro?", (segundoNumero / primeiroNumero) ===  0 )
+// console.log ("O primeiro numero é divisível pelo segundo?", (primeiroNumero % segundoNumero) ===  0 )
+// console.log ("O segundo numero é divisível pelo primeiro?", (segundoNumero % primeiroNumero) ===  0 )
