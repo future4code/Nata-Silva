db6bfd5fc9995efe83256367319b18d4b0fd8f5b
unknown <natanlucas771@gmail.com>
2022-02-21 15:15:14 -0300

    Adição-de-att-objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,99 @@
+//      Exercicio de interpretação:
+
+//  1- a:
+// Matheus Nachtergaele
+// Virginia Cavendish
+// canal: "Globo", horario: "14h"
+
+//  2- :
+//  A: 
+//
+//	nome: "Juca", 
+//	idade: 3, 
+//	raca: "SRD"
+//
+//	nome: "Juba", 
+//	idade: 3, 
+//	raca: "SRD"
+//
+//	nome: "Jubo", 
+//	idade: 3, 
+//	raca: "SRD"
+
+//  B:
+//    A sintaxe dos três pontos antes do nome faz com que 
+//  ele traga os elementos do objeto citado
+//
+//  3- :
+//  A:
+//  
+// FALSE 
+// UNDERFIELD
+// Ele imprime o valor da primeira como false pois ele le
+// o que doi pedido, no segundo nao tem altura entao ele nao consegue achar 
+
+// _________________________________________
+
+// Exercicios de escrita 
+
+// // 1:
+// // A:
+// let pessoa = {
+//     nome: "João",
+//     apelidos: ["jao", "jaozinho", "joaozinho"],
+// } 
+// function nomeDaFuncao (banana) {
+//     console.log (`Eu sou ${banana.nome}, mas pode me chamar de: ${banana.apelidos[0]}, ${banana.apelidos[1]} ou ${banana.apelidos[2]}`)
+// }
+// // B:
+// let pessoa2 = {
+//     ...pessoa,
+//     apelidos:["joaozao", "jhon", "jhonny"],
+// }
+// nomeDaFuncao (pessoa2)
+
+// // 2:
+// // A:
+//
+// let pessoa1 = {
+//     nome: "José",
+//     idade: 40,
+//     profissao: "estudante",
+// }
+
+// let pessoa2 = {
+//     nome: "maria",
+//     idade: 20,
+//     profissao: "estudantee",
+// }
+// // B: 
+// function funcao (pessoaAI) {
+//     return array = [pessoaAI.nome, pessoaAI.nome.length, pessoaAI.idade, pessoaAI.profissao,  pessoaAI.profissao.length]
+// }
+// console.log (funcao (pessoa2))
+
+// 3:
+let carrinho = []
+let fruta1 = {
+    nome: "maçã",
+    disponibilidade: true 
+}
+let fruta2 = {
+    nome: "banana",
+    disponibilidade: true
+}
+let fruta3 = {
+    nome: "abacaxi",
+    disponibilidade: true 
+}
+
+function carrinhoCheio(fruta) {
+    carrinho.push(fruta)
+}
+carrinhoCheio(fruta1)
+carrinhoCheio(fruta2)
+carrinhoCheio(fruta3)
+
+console.log (carrinho)
+
+
